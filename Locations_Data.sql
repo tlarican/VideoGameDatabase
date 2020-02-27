a7e9890a536a909a2e07c37647c3ee379a0d6967
Kylun Robbins <50932388+KylunR@users.noreply.github.com>
2020-02-27 13:56:40 -0800

    Location data queries

---
+++ Locations_Data.sql
@@ -0,0 +1,32 @@
+INSERT INTO LOCATION
+values (1, 0, 'Cyrodiil');
+
+INSERT INTO LOCATION
+values (2, 10, 'High Rock');
+
+INSERT INTO LOCATION
+values (3, 20, 'Morrowind');
+
+INSERT INTO LOCATION
+values (4, 30, 'Cyrodiil');
+
+INSERT INTO LOCATION
+values (5, 40, 'Skyrim');
+
+INSERT INTO LOCATION
+values (6, 50, 'Hammerfell');
+
+INSERT INTO LOCATION
+values (7, 60, 'Valenwood');
+
+INSERT INTO LOCATION
+values (8, 70, 'Elsweyr');
+
+INSERT INTO LOCATION
+values (9, 80, 'Black Marsh');
+
+INSERT INTO LOCATION
+values (10, 90, 'Summerset Isles');
+
+INSERT INTO LOCATION
+values (11, 100, 'Oblivion');

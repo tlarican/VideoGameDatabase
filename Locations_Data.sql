105786bfb62628e5669fe5f0037d6a79186cd36f
tlarican <tlarican@uw.edu>
2020-03-02 23:10:17 -0800

    Changed Location_IDs
    Took 43 minutes

--- Locations_Data.sql
+++ Locations_Data.sql
@@ -1,32 +1,32 @@
 INSERT INTO LOCATION
-values (1, 0, 'Cyrodiil');
+values ('Cyrodiil', 0, 'Cyrodiil');
 
 INSERT INTO LOCATION
-values (2, 10, 'High Rock');
+values ('HighRock', 10, 'High Rock');
 
 INSERT INTO LOCATION
-values (3, 20, 'Morrowind');
+values ('Morrowind', 20, 'Morrowind');
 
 INSERT INTO LOCATION
-values (4, 30, 'Cyrodiil');
+values ('Cyrodiil2', 30, 'Cyrodiil');
 
 INSERT INTO LOCATION
-values (5, 40, 'Skyrim');
+values ('Skyrim', 40, 'Skyrim');
 
 INSERT INTO LOCATION
-values (6, 50, 'Hammerfell');
+values ('Hammerfell', 50, 'Hammerfell');
 
 INSERT INTO LOCATION
-values (7, 60, 'Valenwood');
+values ('Valenwood', 60, 'Valenwood');
 
 INSERT INTO LOCATION
-values (8, 70, 'Elsweyr');
+values ('Elsweyr', 70, 'Elsweyr');
 
 INSERT INTO LOCATION
-values (9, 80, 'Black Marsh');
+values ('BlackMarsh', 80, 'Black Marsh');
 
 INSERT INTO LOCATION
-values (10, 90, 'Summerset Isles');
+values ('SummersetIsles', 90, 'Summerset Isles');
 
 INSERT INTO LOCATION
-values (11, 100, 'Oblivion');
+values ('Oblivion', 100, 'Oblivion');
